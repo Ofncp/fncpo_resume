8f592253d4e9b2d473a909997dcee73a8cc3dbc5
Ofncp <ocampofrancenicolepoticar@gmail.com>
2024-01-07 10:20:25 +0800

    revising js file

--- fncpo_portfolio1/index.js
+++ fncpo_portfolio1/index.js
@@ -1,11 +1,10 @@
-var typed= new Typed(".text", {
-          strings:["Frontend Developer", "Backend Developer", "Full Stack Developer"],
-          typeSpeed:100,
-          backSpeed:100,
-          backDelay:1000,
-          loop: true
-    });
-
+var typed = new Typed(".text", {
+    strings: ["Frontend Developer", "Backend Developer", "Full Stack Developer"],
+    typeSpeed: 100,
+    backSpeed: 100,
+    backDelay: 1000,
+    loop: true
+});
 
 function showSections() {
     var developerToolsSection = document.getElementById("developer-tools");
@@ -13,4 +12,25 @@ function showSections() {
 
     developerToolsSection.style.display = "block";
     contactMeSection.style.display = "block";
+}
+
+function downloadPDF() {
+    // Get the PDF file path
+    var pdfFilePath = './document/Fncpocampo_sd2024.pdf';
+
+    // Create an anchor element
+    var link = document.createElement('a');
+    link.href = pdfFilePath;
+
+    // Set the download attribute to force download
+    link.download = 'Fncpocampo_sd2024.pdf';
+
+    // Append the anchor to the body
+    document.body.appendChild(link);
+
+    // Trigger a click on the anchor element
+    link.click();
+
+    // Clean up: Remove the anchor from the body
+    document.body.removeChild(link);
 }
