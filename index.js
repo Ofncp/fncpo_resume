bd0b75bbd5ef49a01fa095ef26b353bf461b0c14
Ofncp <137748805+Ofncp@users.noreply.github.com>
2024-01-07 10:27:14 +0800

    revising index.js v1
    revising index.js v1

--- index.js
+++ index.js
@@ -1,11 +1,10 @@
-var typed= new Typed(".text", {
-          strings:["Frontend Developer", "Backend Developer", "Full Stack Developer"],
-          typeSpeed:100,
-          backSpeed:100,
-          backDelay:1000,
-          loop: true
-    });
-
+var typed = new Typed(".text", {
+    strings: ["Frontend Developer", "Backend Developer", "Full Stack Developer"],
+    typeSpeed: 100,
+    backSpeed: 100,
+    backDelay: 1000,
+    loop: true
+});
 
 function showSections() {
     var developerToolsSection = document.getElementById("developer-tools");
@@ -16,9 +15,25 @@ function showSections() {
 }
 
 function downloadPDF() {
-            window.open('./document/fncpocampo.pdf', '_blank');
-}
+    // Get the PDF file path
+    var pdfFilePath = './document/Fncpocampo_sd2024.pdf';
 
+    // Create an anchor element
+    var link = document.createElement('a');
+    link.href = pdfFilePath;
+
+    // Set the download attribute to force download
+    link.download = 'Fncpocampo_sd2024.pdf';
+
+    // Append the anchor to the body
+    document.body.appendChild(link);
+
+    // Trigger a click on the anchor element
+    link.click();
+
+    // Clean up: Remove the anchor from the body
+    document.body.removeChild(link);
+}
 
 // email js
 
@@ -51,5 +66,4 @@ function downloadPDF() {
                     alert('There was an error sending your message. Please try again.');
                 });
         });
-
 
